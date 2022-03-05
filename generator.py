2950e38a4f7479eba859a07524c6f4f8f6a07919
GromoJT <85227567+GromoJT@users.noreply.github.com>
2022-03-05 18:25:04 +0100

    Wersja 1.0 Generuje już pełnego jsona bez dawania go do pliku

--- generator.py
+++ generator.py
@@ -1,8 +1,8 @@
 import json
 import random
 
-ResultSize = 50
-
+ResultSize = 10000
+GotowaLista = "{'items':["
 ids = []
 rar = []
 nam = []
@@ -95,12 +95,13 @@ for mod in data1['mod2']:
     mod2Value.append(mod['modValue'])
     mod2Risk.append(mod['modRisk'])
 
+print(mod2Rarity)
 mod2list_len = len(data1["mod2"])
 
 items_len = len(data['baseItems'])
 
 i=0
-while i < items_len:
+while i < mod2list_len:
     if mod2Rarity[i] == "legendary":
         m_legendary.append(mod2id[i])
     elif mod2Rarity[i] =="epic":
@@ -111,6 +112,7 @@ while i < items_len:
         m_normal.append(mod2id[i])
     i = i + 1
 
+print(m_legendary)
 
 i=0
 while i < items_len:
@@ -127,11 +129,11 @@ while i < items_len:
     i = i + 1
 
 
-print("Legendary = ",legendary)
-print("Epic = ",epic)
-print("Rare = ",rare)
-print("Uncommon = ",uncommon)
-print("Common = ",common)
+# print("Legendary = ",legendary)
+# print("Epic = ",epic)
+# print("Rare = ",rare)
+# print("Uncommon = ",uncommon)
+# print("Common = ",common)
 
 
 i = 0
@@ -271,18 +273,38 @@ while i < ResultSize:
         val4.append(round(val3[i] * mod2Value[r2], 2))
         ris4.append(round(ris3[i] * mod2Risk[r2], 2))
     else:
-        r2 = random.choice(m_normal)
-        ids4.append(i)
-        nam4.append(mod2name[r2] + " " + nam3[i])
-        wei4.append(round(wei3[i] * mod2Weight[r2], 2))
-        siz4.append(round(siz3[i] * mod2Size[r2], 2))
-        val4.append(round(val3[i] * mod2Value[r2], 2))
-        ris4.append(round(ris3[i] * mod2Risk[r2], 2))
+        r2 = random.uniform(0.0,1.0)
+        if r2 <0.3:
+            r2 = random.choice(m_normal)
+            nam4.append(mod2name[r2] + " " + nam3[i])
+            ids4.append(i)
+            wei4.append(round(wei3[i] * mod2Weight[r2], 2))
+            siz4.append(round(siz3[i] * mod2Size[r2], 2))
+            val4.append(round(val3[i] * mod2Value[r2], 2))
+            ris4.append(round(ris3[i] * mod2Risk[r2], 2))
+        else:
+            nam4.append(nam3[i])
+            ids4.append(i)
+            wei4.append(round(wei3[i] * 1.0, 2))
+            siz4.append(round(siz3[i] * 1.0, 2))
+            val4.append(round(val3[i] * 1.0, 2))
+            ris4.append(round(ris3[i] * 1.0, 2))
     i = i + 1
 
-print(ids4)
-print(nam4)
-print(wei4)
-print(siz4)
-print(val4)
-print(ris4)
+# print(ids4)
+# print(nam4)
+# print(wei4)
+# print(siz4)
+# print(val4)
+# print(ris4)
+
+#GotowaLista = GotowaLista +"{'TEST':TEST}]}"
+
+i = 0
+while i < ResultSize-1:
+    GotowaLista = GotowaLista+"{'id':'"+str(ids4[i])+"',"+"'name':'"+nam4[i]+"','weight':"+str(wei4[i])+",'size':"+str(siz4[i])+",'value':"+str(val4[i])+",'risk':"+str(ris4[i])+"},"
+    i=i+1
+GotowaLista = GotowaLista+"{'id':'"+str(ids4[ResultSize-1])+"',"+"'name':'"+nam4[ResultSize-1]+"','weight':"+str(wei4[ResultSize-1])+",'size':"+str(siz4[ResultSize-1])+",'value':"+str(val4[ResultSize-1])+",'risk':"+str(ris4[ResultSize-1])+"}]}"
+
+print(json.dumps(GotowaLista, sort_keys=True, indent=4))
+print(GotowaLista)
